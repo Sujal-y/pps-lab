6630a82e56e7693816567e860c2854c8da1c54d7
Sujal-y <sujalyasho2006@gmail.com>
2024-10-13 22:47:04 +0530

    Update 1.c

--- lab1/1.c
+++ lab1/1.c
@@ -1,1 +1,11 @@
-
+//Write a C program to add two integers a and b read through the keyboard.
+//Display the result using third variable sum
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    printf("enter a n b \n");
+    scanf("%d%d",&a,&b);
+    c = a+b;
+    printf("%d",c);
+   return 0;
+}
