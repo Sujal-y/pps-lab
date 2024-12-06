c60a312ac5bb7ca895553fc3bfde77f6c44de984
Sujal-y <sujalyasho2006@gmail.com>
2024-12-06 20:46:17 +0530

    .

--- maths/regula_falsi.c
+++ maths/regula_falsi.c
@@ -1,26 +1,30 @@
 //regula falsi method
 #include<stdio.h>
 #include<math.h>
+float round_off(float value) {
+    return roundf(value * 10000) / 10000;}
+
 float func(float x,float c4,float c3,float c2,float c1,float c0){
     float result = c4*pow(x,4)+c3*pow(x,3)+c2*pow(x,2)+c1*x+c0;
-    return result;
+    return round_off(result);
 }
 float x(float a,float b,float c4,float c3,float c2,float c1,float c0){
-    return (a*func(b,c4,c3,c2,c1,c0)-b*func(a,c4,c3,c2,c1,c0))/(func(b,c4,c3,c2,c1,c0)-func(a,c4,c3,c2,c1,c0));
+    float result = (a*func(b,c4,c3,c2,c1,c0)-b*func(a,c4,c3,c2,c1,c0))/(func(b,c4,c3,c2,c1,c0)-func(a,c4,c3,c2,c1,c0));
+    return round_off(result);
 }
 int main(){
     float c4,c3,c2,c1,c0;
     printf("write the coefficients of the polynomial\n");
     printf("x^4+x^3+x^2+x^1+x^0 ");
     scanf("%f%f%f%f%f",&c4,&c3,&c2,&c1,&c0);
-    float a,b;
+    float a,b,i=1;
     printf("enter the interval\n");
     scanf("%f%f",&a,&b);
     float fa=func(a,c4,c3,c2,c1,c0);
     float fb=func(b,c4,c3,c2,c1,c0);
     float x1=x(a,b,c4,c3,c2,c1,c0);
     float fx1=func(x1,c4,c3,c2,c1,c0);
-    while(fabs(fx1)>0.0001){
+    while(i<=3){
         if(fa*fx1<0){
             b=x1;
             fb=fx1;
@@ -31,9 +35,10 @@ int main(){
         }
         x1=x(a,b,c4,c3,c2,c1,c0);
         fx1=func(x1,c4,c3,c2,c1,c0);
+        printf("the root in %.0f iterration is %.4f\n",i,x1);
+        i++;
     }
-    printf("the root is %f",x1);
-    
-    
+    printf("the root is %.4f\n",x1);
+    return 0;
 
 }
